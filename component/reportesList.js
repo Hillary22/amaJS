2db918a2bc791643c9c704baedc49a79a311868e
Hillary22 <70818275+Hillary22@users.noreply.github.com>
2021-06-14 18:17:51 -0500

    reporte avance

---
+++ component/reportesList.js
@@ -0,0 +1,15 @@
+class reportesList{
+
+    constructor(reporte){
+
+        this.reporte = reporte;
+    }
+
+    render = () => {
+
+         let component = document.createElement('div');
+         component.className = '';
+    }
+
+
+}
---
+++ js/reporte.js
@@ -0,0 +1,47 @@
+const tituloReport = document.getElementById('tituloReport');
+const textoReport = document.getElementById('textoReport');
+const reportBtn = document.getElementById('reportBtn');
+const reportContainer = document.getElementById('reportContainer');
+
+const db = firebase.database();
+const auth = firebase.auth();
+let nombreReport;
+
+
+//  saber usuario
+auth.onAuthStateChanged(
+    (user) =>{
+        db.ref('users/registrados/'+user.uid).once(
+            'value',
+            (data)=>{
+                let userDB = data.val();
+                nombreReport = userDB.nombre;
+            }
+        );
+        db.ref('reportes').on('value',function(data) {
+            reportContainer.innerHTML = '';
+            data.forEach(
+                report => {
+                    let userDB = report.val();
+                    let fila = new reportesList(userDB);
+                    reportContainer.appendChild(fila.render());
+                }
+            );  
+        } 
+        );
+    }
+);
+
+
+publiBtn.addEventListener('click', ()=>{
+    let referencia = db.ref('reportes').push();
+    let r = {
+        
+        id: referencia.key,
+        nombre: nombreReport,
+        titulo: tituloReport.value,
+        reporte: textoReport.value,
+    };
+    referencia.set(r);
+});
+
