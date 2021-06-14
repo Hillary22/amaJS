39737cd951b5e16acb3e19b28bcc1fefe8bdeae0
Hillary22 <70818275+Hillary22@users.noreply.github.com>
2021-06-14 17:17:24 -0500

    css consejos

--- js/login.js
+++ js/login.js
@@ -4,12 +4,25 @@ const correo = document.getElementById('correo');
 const contraseña = document.getElementById('contraseña');
 const ingreBtn = document.getElementById('ingreBtn');
 
+
+auth.onAuthStateChanged(
+ //si ya esta logeado pasa de una a index.html
+    (user) =>{
+        if (user !== null){
+            window.Location.href= "index.html";
+        }
+    }
+
+);
+
+
 ingreBtn.addEventListener('click', () =>{
 
+    
     auth.signInWithEmailAndPassword(correo.value, contraseña.value).then(
 
         (data)=>{
-            window.location.href ='perfil.html';
+            window.location.href ="index.html";
         }
     ).catch(
         (error)=>{
--- js/user.js
+++ js/user.js
@@ -12,18 +12,23 @@ const auth = firebase.auth();
 auth.onAuthStateChanged(
     (user) =>{
 
-        console.log(user.uid);
-        database.ref('users/registrados/'+user.uid).once(
-            'value',
-            (data)=>{
-                let userDB = data.val();
-                saludo.innerHTM = 'Hola,'+userDB.nombre;
-                nombre.innerHTM = userDB.nombre;
-                correo.innerHTM = userDB.correo;
-                ciudad.innerHTM = userDB.ciudad;
-                
-            }
-        );
-        
+        if(user!== null){
+
+      
+            console.log(user.uid);
+            database.ref('users/registrados/'+user.uid).once(
+                'value',
+                (data)=>{
+                    let userDB = data.val();
+                    saludo.innerHTM = '¡Hola!';
+                    nombre.innerHTM = userDB.nombre;
+                    correo.innerHTM = userDB.correo;
+                    ciudad.innerHTM = userDB.ciudad;
+                    
+                }
+            );
+        }else{
+            window.Location.href="login.html";
+        }
     }
 );
